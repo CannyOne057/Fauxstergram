3a909290c4a465c8e39458bd22511739dd3f91e3
Bjorkbat <bjorkbat@gmail.com>
2017-03-17 17:16:12 -0600

    Added login / logout

--- app/src/main/java/me/bjorkbat/fauxstergram/LogoutFragment.java
+++ app/src/main/java/me/bjorkbat/fauxstergram/LogoutFragment.java
@@ -2,10 +2,17 @@ package me.bjorkbat.fauxstergram;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 /**
  * Created by johnborden on 3/17/17.
@@ -13,10 +20,31 @@ import android.view.ViewGroup;
 
 public class LogoutFragment extends Fragment {
 
+    Button mLogoutButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_logout, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_logout, container, false);
+
+        mLogoutButton = (Button) rootView.findViewById(R.id.logout);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO: figure out why the hell signout is deprecated
+                AuthUI.getInstance()
+                        .signOut(getActivity())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getActivity(), "You've Logged Out", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/me/bjorkbat/fauxstergram/MainActivity.java
+++ app/src/main/java/me/bjorkbat/fauxstergram/MainActivity.java
@@ -1,17 +1,22 @@
 package me.bjorkbat.fauxstergram;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 123;
+    private static final int RC_SIGN_IN = 1;
 
     FauxstergramPagerAdapter mFauxstergramPagerAdapter;
 
@@ -41,6 +46,40 @@ public class MainActivity extends AppCompatActivity {
                                     new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
                             .build(),
                     RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+
+                // TODO: get the feed
+
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, R.string.sign_in_cancelled, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, R.string.sign_in_no_connection, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, R.string.sign_in_weird, Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
         }
     }
 }
